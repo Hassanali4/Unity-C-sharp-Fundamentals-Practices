97590971b760e4e6a2d23c807a45829a57bd8383
Hassanali4 <89867379+Hassanali4@users.noreply.github.com>
2023-01-13 18:23:21 +0500

    Visited after some time
    To Refreshed the concepts

---
+++ Unity Classes(SurvivalGuide[8])/Assets/MoveWithTime.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MoveWithTime : MonoBehaviour
+{
+    [SerializeField] private int _timeAfterChangingDirection;// Time if will take to change the direction
+    [SerializeField] private int _direction;//an integer variable to adjust the sign of positive & negative after certain amount of time
+    [SerializeField] private float _moveSpeed = 2;// speed of movemnt
+    [SerializeField] private int count;// the internal counter that counts after how much certain amount it will reecet the movement by checking with _timeAfterChangingDirection Variable
+    
+    void Start()
+    { 
+        _direction = 1;
+    }
+
+    void Update()
+    {
+        count += 1;
+            this.transform.position = this.transform.position + new Vector3(_direction * _moveSpeed * Time.deltaTime, _direction * _moveSpeed * Time.deltaTime, 0f);
+        if (count > _timeAfterChangingDirection)
+        {
+            count = 0;
+            _direction = -1 * _direction;
+        }
+
+    }
+}
